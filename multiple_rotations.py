5f43a8b41faf89352fd98c3ac9b3d0600264570b
Ryan <taylor48@my.erau.edu>
2022-10-14 12:20:13 -0400

    more changes

--- multiple_rotations.py
+++ multiple_rotations.py
@@ -5,14 +5,14 @@ from sympy import *
 
 E = CoordSys3D('E')
 theta = Symbol('theta')
+phi = Symbol('phi')
 psi = Symbol('psi')
 
-A = E.orient_new_axis('A', psi, E.j)
-B = A.orient_new_axis('B', theta, A.i)
+A = E.orient_new_axis('A', theta, E.i)
+B = A.orient_new_axis('B', phi, A.j)
+C = B.orient_new_axis('C', psi, B.j)
 
-rot_mat = B.rotation_matrix(E)
+rot_mat = C.rotation_matrix(E)
 rot_mat = rot_mat.transpose()
-
-q = B.quaternion(E)
 
 pprint(rot_mat)
